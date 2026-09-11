fe5297ee2dd8a671db8df8dc637a61b9fd9ad1c3
JuanJo <juancorreap07@gmail.com>
2026-09-11 16:00:55 -0500

    Creation of class person and advancements in class Number

---
+++ src/Number.java
@@ -0,0 +1,13 @@
+public class Number {
+    private int value;
+
+    public Number(int value) {
+        this.value = value;
+    }
+
+    public int getVAlue() {
+        return value;
+    }
+
+    public void setValue
+}
--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,20 @@
-class person {
+class Person {
     private String name;
 
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
